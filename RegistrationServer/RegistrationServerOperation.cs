06be3c9ffdd9bc9075909d9bed0602ee23294a02
Martin Leeb <m.leeb.it@gmail.com>
2020-11-07 17:05:56 +0100

    Initial commit

---
+++ RegistrationServer/RegistrationServerOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistrationServer
+{
+    public enum RegistrationServerOperation
+    {
+        Write = 0,
+        Read,
+        Join,
+        Leave,
+    }
+
+    public static class RegistratioServerOperationExtension
+    {
+        public static int Value(this RegistrationServerOperation operation)
+            => Convert.ToInt32(operation);
+
+        public static RegistrationServerOperation toRegistrationServerOperation(this int operation)
+            => (RegistrationServerOperation)operation;
+    }
+}
